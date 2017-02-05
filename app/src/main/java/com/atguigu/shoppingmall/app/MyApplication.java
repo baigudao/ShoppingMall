67857856de6fa1b86f53ca7c11c855fa9bb7b1d5
jackie <971060378@qq.com>
2017-02-05 12:32:34 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/shoppingmall/app/MyApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.shoppingmall.app;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    private static Context mContext;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        this.mContext = this;
+    }
+    // 获取全局上下文
+    public static Context getContext() {
+        return mContext;
+    }
+}
